c39b696d787e81189b8eff9d216c2b187408ce77
slscott9 <59343900+slscott9@users.noreply.github.com>
2020-08-18 07:21:37 -0500

    currently gets the location for new cemetery location edit text. but needs to cut the extra from the address array

---
+++ app/src/main/java/hfad/com/newestcemeteryproject/MainActivity.kt
@@ -0,0 +1,205 @@
+package hfad.com.newestcemeteryproject
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+/**
+ * Location sample.
+ *
+ *
+ * Demonstrates use of the Location API to retrieve the last known location for a device.
+ */
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * Provides the entry point to the Fused Location Provider API.
+     */
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    private val geocoder: Geocoder = Geocoder(this, Locale.getDefault())
+    private lateinit var adressList: List<Address>
+
+
+    /**
+     * Represents a geographical location.
+     */
+    protected var mLastLocation: Location? = null
+
+    private var mLatitudeLabel: String? = null
+    private var mLongitudeLabel: String? = null
+    private var mLatitudeText: TextView? = null
+    private var mLongitudeText: TextView? = null
+
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        textView.setOnClickListener {
+            textView.text = adressList[0].toString()
+        }
+
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        if (!checkPermissions()) {
+            requestPermissions()
+        } else {
+            getLastLocation()
+        }
+    }
+
+    /**
+     * Provides a simple way of getting a device's location and is well suited for
+     * applications that do not require a fine-grained location and that do not need location
+     * updates. Gets the best and most recent location currently available, which may be null
+     * in rare cases when a location is not available.
+     *
+     *
+     * Note: this method should be called after location permission has been granted.
+     */
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        mFusedLocationClient!!.lastLocation
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful && task.result != null) {
+                    mLastLocation = task.result
+
+                    val addressList: List<Address>? = geocoder.getFromLocation(mLastLocation!!.latitude,
+                        mLastLocation!!.latitude, 1)
+
+
+                    mLatitudeText!!.setText(
+                        mLatitudeLabel+":   "+
+                                (mLastLocation )!!.latitude)
+                    mLongitudeText!!.setText(mLongitudeLabel+":   "+
+                            (mLastLocation )!!.longitude)
+                } else {
+
+                }
+            }
+    }
+
+
+
+
+    private fun showSnackbar(mainTextStringId: Int, actionStringId: Int,
+                             listener: View.OnClickListener) {
+
+        Toast.makeText(this@MainActivity, getString(mainTextStringId), Toast.LENGTH_LONG).show()
+    }
+
+    /**
+     * Return the current state of the permissions needed.
+     */
+    private fun checkPermissions(): Boolean {
+        val permissionState = ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION)
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(this@MainActivity,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_PERMISSIONS_REQUEST_CODE)
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.")
+
+            showSnackbar(R.string.permission_rationale, android.R.string.ok,
+                View.OnClickListener {
+                    // Request permission
+                    startLocationPermissionRequest()
+                })
+
+        } else {
+            Log.i(TAG, "Requesting permission")
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            startLocationPermissionRequest()
+        }
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        Log.i(TAG, "onRequestPermissionResult")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.size <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.")
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                getLastLocation()
+            } else {
+                // Permission denied.
+
+                // Notify the user via a SnackBar that they have rejected a core permission for the
+                // app, which makes the Activity useless. In a real app, core permissions would
+                // typically be best requested during a welcome-screen flow.
+
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                    View.OnClickListener {
+                        // Build intent that displays the App settings screen.
+                        val intent = Intent()
+                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                        val uri = Uri.fromParts(
+                            "package",
+                            BuildConfig.APPLICATION_ID, null
+                        )
+                        intent.data = uri
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+
+                    }
+
+            }
+        }
+    }
+
+    companion object {
+
+        private val TAG = "LocationProvider"
+
+        private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+    }
+}
--- app/src/main/java/hfad/com/newestcemeteryproject/NewCemeteryActivity.kt
+++ app/src/main/java/hfad/com/newestcemeteryproject/NewCemeteryActivity.kt
@@ -1,20 +1,50 @@
 package hfad.com.newestcemeteryproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Transformations.map
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.*
+
 import hfad.com.newestcemeteryproject.data.Cemetery
 import hfad.com.newestcemeteryproject.databinding.ActivityNewCemeteryBinding
 import hfad.com.newestcemeteryproject.viewmodel.CemeteryViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_new_cemetery.*
+import java.util.*
+
 
 class NewCemeteryActivity : AppCompatActivity() {
 
-    private lateinit var editWordView: EditText
     private lateinit var viewModel: CemeteryViewModel
     private lateinit var binding: ActivityNewCemeteryBinding
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+
+    private lateinit var  geocoder: Geocoder
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +53,15 @@ class NewCemeteryActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(CemeteryViewModel::class.java)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest()
+
+        geocoder = Geocoder(this, Locale.getDefault())
+
+
+        binding.locationEditText.setOnClickListener {
+
+        }
 
         binding.addCemButton.setOnClickListener {
             if (binding.nameEditText.text.isNullOrEmpty() ||
@@ -33,7 +72,8 @@ class NewCemeteryActivity : AppCompatActivity() {
                 binding.rangeEditText.text.isNullOrEmpty() ||
                 binding.sectionEditText.text.isNullOrEmpty() ||
                 binding.gpsEditText.text.isNullOrEmpty() ||
-                binding.firstYearEditText.text.isNullOrEmpty()) {
+                binding.firstYearEditText.text.isNullOrEmpty()
+            ) {
                 Toast.makeText(this, "Please entery all fields", Toast.LENGTH_SHORT).show()
             } else {
 
@@ -48,7 +88,8 @@ class NewCemeteryActivity : AppCompatActivity() {
                 val gps = binding.gpsEditText.text.toString()
                 val firstYear = binding.firstYearEditText.text.toString()
                 val cemetery =
-                    Cemetery(cemeteryName = name,
+                    Cemetery(
+                        cemeteryName = name,
                         cemeteryLocation = location,
                         cemeteryState = state,
                         cemeteryCounty = county,
@@ -57,15 +98,58 @@ class NewCemeteryActivity : AppCompatActivity() {
                         section = section,
                         spot = spot,
                         gps = gps,
-                        firstYear = firstYear)
+                        firstYear = firstYear
+                    )
                 viewModel.insertCemetery(cemetery)
                 finish()
+            }
+        }
 
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations){
+                    // Update UI with location data
+                    // ...
 
+                    val addressList: List<Address>? = geocoder.getFromLocation(location.latitude, location.longitude, 1) //converts lattitude and longitude to an address
+
+                    /*
+                            USE THE CODE FROM HAPPY PLACES TO CONVERT THE ADDRESS AND TAKE OUT THE EXTRA STUFF IN THE ARRAY
+                     */
+
+                    binding.locationEditText.setText(addressList?.get(0).toString()) //set the text to the adress
+
+                }
             }
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+
+    }
+
+    private fun isLocationEnabled(): Boolean { //not sure if needed
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    @SuppressLint("MissingPermission") //need to put a permission dialog that can take user to the location settings
+    private fun startLocationUpdates() {
+        fusedLocationClient?.requestLocationUpdates(locationRequest,
+            locationCallback,
+            Looper.getMainLooper())
+    }
+
 
 }
+
+
+
+
 
